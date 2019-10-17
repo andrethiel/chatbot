0cf5b35cd5c76504a5c73e9e545be423e58e2fc6
André Luiz Thiel <andre_thiel007@hotmail.com>
2019-10-17 19:26:52 -0300

    novo

--- Chat/scripts/chat-rocio.js
+++ Chat/scripts/chat-rocio.js
@@ -1,35 +1,75 @@
 var botui = new BotUI('hello-world');
 botui.message.bot({ // show first message
-    delay: 200,
+    loading: true,
+    delay: 1000,
     content: 'Olá eu sou o Bot Rocio 👋'
 }).then(() => {
     return botui.message.bot({ // second one
-        delay: 1000, // wait 1 sec.
-        content: 'Como posso te ajudar ??'
+        loading: true,
+        delay: 1500, // wait 1 sec.
+        content: 'Estou aqui para lhe ajudar com as perguntas mais frequentes sobre o hospital'
+    })
+}).then(() => {
+    return botui.message.bot({ // second one
+        loading: true,
+        delay: 1500, // wait 1 sec.
+        content: 'Divimos as perguntas em alguns grupos para melhor lhe atendelo'
     })
 }).then(() => {
     return botui.action.button({
+        loading: true,
         delay: 1000,
         action: [
             {
-                text: 'Informções sobre pacientes',
-                value: 'informacoes'
+                text: 'Pronto Socorro',
+                value: 'pronto'
             },
             {
-                text: 'Horarios de Visita',
-                value: 'horarios'
+                text: 'Centro de Imagens',
+                value: 'imagens'
+            },
+            {
+                text: 'Recepcoes',
+                value: 'recepcoes'
             }
         ]
     })
 }).then(res => {
     switch (res.value) {
-        case "informacoes":
-            botui.message.bot({
-                delay: 200,
+        case "pronto":
+            return botui.action.button({
                 loading: true,
-                content: 'O hospital não passa nenhuma informação sobre os nossos pacientes 😔'
-            }).then(() => {
-                inicio();
+                delay: 1000,
+                action: [
+                    {
+                        text: 'Convenios atendidos',
+                        value: 'convenios'
+                    },
+                    {
+                        text: 'Consultar particulares',
+                        value: 'consulta'
+                    },
+                    {
+                        text: 'Atendimento SUS',
+                        value: 'sus'
+                    }
+                ]
+            }).then(res => {
+                switch (res.value) {
+                    case "convenios":
+                        botui.message.bot({
+                            delay: 1000,
+                            loading: true,
+                            content: 'Atendemos os seguintes convenios'
+                        })
+                        botui.message.bot({
+                            delay: 1500,
+                            loading: true,
+                            content: '<b>Amil, Unimed, Cassi, Saude Caixa, Fundação Copel, Fundação Sanepar'+
+                            'Bradesco Saúde, Siemaco, Voam, Sul America, GoBox</b>'
+                        })
+                        break;
+                }
             })
             break;
         case "horarios":
@@ -41,8 +81,8 @@ botui.message.bot({ // show first message
             botui.message.bot({
                 delay: 2000,
                 loading: true,
-                content: 'Os quartos do primeiro andar são das <b>14h00 às 15h00</b> e do '+
-                'segunda andar das <b>16h00 às 17h00</b>'
+                content: 'Os quartos do primeiro andar são das <b>14h00 às 15h00</b> e do ' +
+                    'segunda andar das <b>16h00 às 17h00</b>'
             })
             break;
         default:
---
+++ Chat/scripts/chat-rocio_.js
@@ -0,0 +1,6 @@
+var botui = new BotUI('hello-world');
+botui.message.bot({ // show first message
+    loading: true,
+    delay: 1000,
+    content: 'Olá eu sou o Bot Rocio 👋'
+})
