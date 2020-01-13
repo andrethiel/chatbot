f7fe5d879ccdf86fd980c250bca458f56c55f505
André Luiz Thiel <andre_thiel007@hotmail.com>
2020-01-12 23:31:30 -0300

    novo

--- Chat/scripts/chat-rocio.js
+++ Chat/scripts/chat-rocio.js
@@ -20,7 +20,7 @@ botui.message.bot({
 })
 
 
-var loop = function(){
+var loop = function () {
     return botui.action.button({
         loading: true,
         delay: 1000,
@@ -41,65 +41,93 @@ var loop = function(){
     }).then(res => {
         switch (res.value) {
             case "pronto":
-                return botui.action.button({
-                    loading: true,
-                    delay: 1000,
-                    action: [
-                        {
-                            text: 'Convenios atendidos',
-                            value: 'convenios'
-                        },
-                        {
-                            text: 'Consultar particulares',
-                            value: 'consulta'
-                        },
-                        {
-                            text: 'Atendimento SUS',
-                            value: 'sus'
-                        }
-                    ]
-                }).then(res => {
-                    switch (res.value) {
-                        case "convenios":
-                            botui.message.bot({
-                                delay: 1000,
-                                loading: true,
-                                content: 'Atendemos os seguintes convenios'
-                            })
-                            botui.message.bot({
-                                delay: 1500,
-                                loading: true,
-                                content: '<b>Amil, Unimed, Cassi, Saude Caixa, Fundação Copel, Fundação Sanepar' +
-                                    'Bradesco Saúde, Siemaco, Voam, Sul America, GoBox</b>'
-                            })
-                            return botui.action.button({
-                                loading: true,
-                                delay: 1000,
-                                action: [
-                                    {
-                                        text: 'Mais ajuda',
-                                        value: '1'
-                                    },
-                                    {
-                                        text: 'Nao',
-                                        value: '0'
+                    return botui.action.button({
+                        loading: true,
+                        delay: 1000,
+                        action: [
+                            {
+                                text: 'Convenios atendidos',
+                                value: 'convenios'
+                            },
+                            {
+                                text: 'Consultar particulares',
+                                value: 'consulta'
+                            },
+                            {
+                                text: 'Atendimento SUS',
+                                value: 'sus'
+                            }
+                        ]
+                    }).then(res => {
+                        switch (res.value) {
+                            case "convenios":
+                                botui.message.bot({
+                                    delay: 1000,
+                                    loading: true,
+                                    content: 'Atendemos os seguintes convenios'
+                                })
+                                botui.message.bot({
+                                    delay: 1500,
+                                    loading: true,
+                                    content: '<b>Amil, Unimed, Cassi, Saude Caixa, Fundação Copel, Fundação Sanepar' +
+                                        'Bradesco Saúde, Siemaco, Voam, Sul America, GoBox</b>'
+                                })
+                                return botui.action.button({
+                                    loading: true,
+                                    delay: 1000,
+                                    action: [
+                                        {
+                                            text: 'Mais ajuda',
+                                            value: '1'
+                                        },
+                                        {
+                                            text: 'Nao',
+                                            value: '0'
+                                        }
+                                    ]
+                                }).then(res => {
+                                    if (res.value == '0') {
+                                        botui.message.bot({ // show first message
+                                            delay: 200,
+                                            content: 'fim atendimento'
+                                        })
+                                    } else {
+                                        loop();
                                     }
-                                ]
-                            }).then(res => {
-                                if(res.value == '0'){
-                                    botui.message.bot({ // show first message
-                                        delay: 200,
-                                        content: 'fim atendimento'
-                                    })
-                                }else{
-                                    loop();
+                                })
+                            break
+                        }
+                
+                    })
+                break
+                case 'recepcoes':
+                        return botui.action.button({
+                            loading: true,
+                            delay: 1000,
+                            action: [
+                                {
+                                    text: 'Telefone do ambulatório',
+                                    value: 'ambu'
+                                },
+                                {
+                                    text: 'Telefone para marcação de consultas  da UNIMED',
+                                    value: 'uni'
+                                },
+                                {
+                                    text: 'Telefone para marcação de exames de imagem',
+                                    value: 'cdi'
+                                },{
+                                    text: 'Telefone do setor de endoscopia',
+                                    value: 'endos'
+                                },{
+                                    text: 'Como o paciente com problemas no pós cirúrgico faz para retornar na emergência',
+                                    value: 'endos'
                                 }
-
-                            })
-
-                            break;
-                    }
-                })
-        }
+                            ]
+                        })
+                    break
+                    case 'dedede':
+                        break
+            }
     })
 }
